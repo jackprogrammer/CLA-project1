d5e7e3452adbc8abee78b5ef63274a5d9efefa2e
Phan Quan <phanquan012@gmail.com>
2020-05-27 10:01:31 +0700

    update admin

---
+++ CLA.Data/ClaContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLA.Data
+{
+    public partial class ClaContext : DbContext
+    {
+        public ClaContext()
+        {
+        }
+
+        public ClaContext(DbContextOptions<ClaContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                
+            }
+        }
+    }
+}
